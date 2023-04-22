636f5ef9728dc01d47a6812b8827fcb15eec3aea
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2023-04-22 01:51:46 -0300

    (feat): reimplement v11 addField

--- src/main/discord/utils/RichEmbed.ts
+++ src/main/discord/utils/RichEmbed.ts
@@ -4,6 +4,10 @@ export class RichEmbed extends EmbedBuilder {
   public addBlakField(inline = false): this {
     return this.addFields({ name: '\u200B', value: '\u200B', inline });
   }
+
+  public addField(name: string, value: string, inline = false): this {
+    return this.addFields({ name, value, inline });
+  }
 }
 
 export class CloneableRichEmbed extends RichEmbed {
