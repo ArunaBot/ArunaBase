7c009820fe3548476b23f6675954df95b55a1193
Lobo Metalúrgico <43734867+LoboMetalurgico@users.noreply.github.com>
2025-02-22 19:54:31 -0300

    (fix): buttons are replaced but not deregistered

--- src/main/discord/utils/Patches.ts
+++ src/main/discord/utils/Patches.ts
@@ -1,4 +1,4 @@
-import { Message, OmitPartialGroupDMChannel, ActionRowBuilder, ButtonBuilder } from 'discord.js';
+import { Message, OmitPartialGroupDMChannel, ActionRowBuilder, ButtonBuilder, ActionRow, ComponentType } from 'discord.js';
 import { ButtonStructure } from '../structures';
 import { ButtonManager } from '../managers';
 
@@ -9,6 +9,17 @@ declare module 'discord.js' {
 }
 
 Message.prototype.setButtons = function(buttons: ButtonStructure[]): Promise<OmitPartialGroupDMChannel<Message<any>>> {
+  if (this.components.length > 0) {
+    this.components.forEach((component) => {
+      if (component instanceof ActionRow) {
+        component.components.forEach((c) => {
+          if (c.type === ComponentType.Button) {
+            ButtonManager.getInstance().unregisterButton(c.customId!);
+          }
+        });
+      }
+    });
+  }
   ButtonManager.getInstance().registerButtons(buttons);
   const row = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons.map((b) => b.getButton()));
   return this.edit({ content: this.content, embeds: this.embeds, components: [row] });
